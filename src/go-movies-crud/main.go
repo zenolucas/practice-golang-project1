c8c2591aa4e1486237a0040d4be78551904ee25c
Zen Lucas <2200501@slu.edu.ph>
2023-01-24 07:39:58 +0800

    create functions, createMovie() and updateMovie()

--- src/go-movies-crud/main.go
+++ src/go-movies-crud/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Movie struct {
@@ -24,7 +26,10 @@ var movies []Movie
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+	err := json.NewEncoder(w).Encode(movies)
+	if err != nil {
+		return
+	}
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +37,15 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range movies {
 		if item.ID == params["id"] {
+			// how to delete by append
 			movies = append(movies[:index], movies[index+1:]...)
 			break
 		}
+	}
+	// after deleting a movie, return all movies
+	err := json.NewEncoder(w).Encode(movies)
+	if err != nil {
+		return
 	}
 }
 
@@ -43,7 +54,42 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, item := range movies {
 		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
+			err := json.NewEncoder(w).Encode(item)
+			if err != nil {
+				return
+			}
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	err := json.NewEncoder(w).Encode(movie)
+	if err != nil {
+		return
+	}
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			err := json.NewEncoder(w).Encode(movie)
+			if err != nil {
+				return
+			}
 			return
 		}
 	}
